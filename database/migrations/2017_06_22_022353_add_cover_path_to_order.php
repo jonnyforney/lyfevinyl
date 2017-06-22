ed7d7681ce30353405e9809490978c4b8c315e4a
Jonny <jonnyforney@Jonnys-MacBook-Pro.local>
2017-06-21 23:25:40 -0400

    create/update song - attach a song to an order - update a track path - can attach multiple orders at a time - tests for above functionality

---
+++ database/migrations/2017_06_22_022353_add_cover_path_to_order.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddCoverPathToOrder extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('orders', function (Blueprint $table) {
+            $table->string('front_cover_path')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('orders', function (Blueprint $table) {
+            //
+        });
+    }
+}
--- tests/Unit/OrderTest.php
+++ tests/Unit/OrderTest.php
@@ -4,6 +4,8 @@ namespace Tests\Unit;
 
 use Tests\TestCase;
 use App\User;
+use App\Order;
+use App\Song;
 use Illuminate\Foundation\Testing\DatabaseMigrations;
 use Illuminate\Foundation\Testing\DatabaseTransactions;
 
@@ -39,4 +41,58 @@ class OrderTest extends TestCase
             ]);
     } 
 
+    /** @test */
+    public function an_save_a_cover_path()
+    {
+        $order = factory(Order::class)->create(['id' => '17AA00001']);
+
+        $path = '/path/to/cover/image';
+
+        $order->store(['front_cover_path' => $path]);
+        
+        $this->assertEquals($order->front_cover_path, $path);        
+    }
+
+    /** @test */
+    public function can_save_a_song()
+    {
+        $path = '/path/to/song/file';        
+
+        $order = factory(Order::class)->create(['id' => '17AA00001']);
+        $song = factory(Song::class)->create(['order_id' => $order->id, 'path' => $path, 'track' => 1, 'side' => 'A']); 
+
+        Song::store($order, [['path' => $path.'/2', 'track' => 2, 'side' => 'A']]);
+
+        $this->assertEquals(2, count($order->songs));        
+    }
+
+    /** @test */
+    public function can_update_a_song()
+    {
+        $path = '/path/to/song/file';        
+
+        $order = factory(Order::class)->create(['id' => '17AA00001']);
+        $song = factory(Song::class)->create(['order_id' => $order->id, 'path' => $path, 'track' => 1, 'side' => 'A']); 
+
+        Song::store($order, [['path' => $path.'/update', 'track' => 1, 'side' => 'A']]);
+
+        $this->assertEquals(1, count($order->songs));        
+    }
+
+    /** @test */
+    public function can_save_multiple_song()
+    {
+        $path = '/path/to/song/file';        
+
+        $order = factory(Order::class)->create(['id' => '17AA00001']);        
+
+        Song::store($order, 
+                    [
+                        ['path' => $path.'/1', 'track' => 1, 'side' => 'A'],
+                        ['path' => $path.'/2', 'track' => 2, 'side' => 'A']
+                    ]
+        );
+        
+        $this->assertEquals(2, count($order->songs));        
+    }
 }
