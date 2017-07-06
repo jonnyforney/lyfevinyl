0b2877e6062620522f5a7dfbb470fafecfe8a5f2
Jake Brokaw <jake.brokaw@formstack.com>
2017-07-05 19:03:02 -0500

    dropzone stylezzz

--- resources/views/steps.blade.php
+++ resources/views/steps.blade.php
@@ -3,15 +3,15 @@
 @section('content')
 
   <div id="app" v-cloak>
-    <div id="progress-bar" v-cloak>
-      <progress-bar percent="25"></progress-bar>
-    </div>
-    <div id="steps" v-cloak>
-      <steps
-        customer_id="{{ session('customer_id') }}"
-        is_logged_in="{{ Auth::check() }}"
-      ></steps>
-    </div>
+      <div id="progress-bar" v-cloak key="progress">
+        <progress-bar percent="25"></progress-bar>
+      </div>
+      <div id="steps" v-cloak key="steps">
+        <steps
+          customer_id="{{ session('customer_id') }}"
+          is_logged_in="{{ Auth::check() }}"
+        ></steps>
+      </div>
   </div>
 
 @endsection
