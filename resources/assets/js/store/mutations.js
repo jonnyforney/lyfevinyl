fde0c3cd31691217a73081d50f7deeb040640567
Jonny <jonnyforney@Jonnys-MacBook-Pro.local>
2017-07-09 13:46:32 -0400

    finish modularizing

--- resources/assets/js/store/mutations.js
+++ resources/assets/js/store/mutations.js
@@ -1,17 +1,3 @@
 module.exports = {
-    setCustomerId(state, id) {
-        state.customer_id = id;
-    },
-    setOrderId(state, id) {
-        state.order_id = id;
-    },
-    setName(state, name) {
-        state.name = name;
-    },
-    setSong(state, sides) {
-        state.sides = sides;
-    },
-    setFrontCoverPath(state, path) {
-        state.front_cover_path = path;
-    }
+
 }
