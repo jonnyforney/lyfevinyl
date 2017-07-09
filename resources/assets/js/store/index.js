4fdb96c18a00f73c70c1b86b8477725aa2a1140b
Jonny <jonnyforney@Jonnys-MacBook-Pro.local>
2017-07-09 13:46:32 -0400

    finish modularizing

--- resources/assets/js/store/index.js
+++ resources/assets/js/store/index.js
@@ -4,6 +4,7 @@ import actions from './actions'
 import getters from './getters'
 import mutations from './mutations'
 import display from './modules/display'
+import order from './modules/order'
 
 Vue.use(Vuex);
 
@@ -12,92 +13,11 @@ export default new Vuex.Store({
     getters,
     mutations,
     modules: {
-        display
+        display,
+        order
     },
     state: {
         "customer_id": null,
-        "order_id": null,
-        "name": "",
-        "front_cover_path": "",
-        "sides": [{
-                "side": "a",
-                "songs": [{
-                        "file": "",
-                        "side": "",
-                        "track": "",
-                        "picked": false
-                    },
-                    {
-                        "file": "",
-                        "picked": false
-                    },
-                    {
-                        "file": "",
-                        "picked": false
-                    },
-                    {
-                        "file": "",
-                        "picked": false
-                    },
-                    {
-                        "file": "",
-                        "picked": false
-                    }
-                ]
-            },
-            {
-                "side": "b",
-                "songs": [{
-                        "file": "",
-                        "picked": false
-                    },
-                    {
-                        "file": "",
-                        "picked": false
-                    },
-                    {
-                        "file": "",
-                        "picked": false
-                    },
-                    {
-                        "file": "",
-                        "picked": false
-                    },
-                    {
-                        "file": "",
-                        "picked": false
-                    }
-                ]
-            }
-        ],
-        "steps": [
-            "album_name",
-            "songs",
-            "covers",
-            "preview"
-        ],
-        "current_step": "album_name",
-        "progress": 0,
-        "step_album_name": {
-            "headline": "Let's get started!",
-            "subhead": "What's the best way to start personalizing your vinyl? Giving it a title, of course! You can choose to have this name on the front of your album cover.",
-            "show": true
-        },
-        "step_songs": {
-            "headline": "",
-            "subhead": "Let's upload the songs now. <br>Make sure you own the music and it must be in .mp3 format.",
-            "show": false
-        },
-        "step_covers": {
-            "headline": "",
-            "subhead": "This is going to be a wonderful record. <br>Let's add some images for the album art now.",
-            "show": false
-        },
-        "step_preview": {
-            "headline": "Let's preview your vinyl.",
-            "subhead": "Double check that everything is looking swell. Feel free to go back and change anything before moving forward.",
-            "show": false
-        }
-
+        "order_id": null
     }
 });
